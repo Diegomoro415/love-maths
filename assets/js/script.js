1831aed22c7258620b3e883d8b4932b0147800a6
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-16 21:09:40 +0100

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,29 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You Clicked Submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                    alert(`You Clicked ${gameType}`);
+                    runGame(gameType);
                 
             }
         })
     }
+    runGame("addition");
 })
 
 /**
  * Teh main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGAme() {
+function runGame(gameType) {
     // creates two random numbers berween 1 and 25
-    let number1 = Math.floor(Math.random() * 25) + 1;
-    let number2 = Math.floor(Math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gametype}`);
+        throw `Unknown game type: ${gametype}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -43,8 +51,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
